baf98523acb839272eb1347e69d64eecc7ab775a
lautaroda <lautarodeangelis@gmail.com>
2024-08-13 15:13:46 -0300

    Upgrade Odoo 17

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'CRM Team Annual Target',
-    'version': '15.0.1.0.0',
+    'version': '17.0.1.0.0',
     'summary': 'Modifica el campo invoiced_target para que sea anual',
     'description': 'Este módulo cambia el campo invoiced_target en crm.team para que represente una meta anual en lugar de mensual.',
     'author': 'Lautaro De Angelis',
